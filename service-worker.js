77f6f4f696b47310ec648aa0f390b6544353285b
aminhosseini7 <amin.h76@gmail.com>
2025-12-05 21:19:08 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open("neon-vocab-cache-v1").then(cache => {
+    caches.open("neon-vocab-cache-v2").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
